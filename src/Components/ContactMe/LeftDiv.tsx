166def8c1c84a81cf207c999385f6a22252fdfa2
mohammadjury <mohammadjury00@gmail.com>
2024-05-05 14:17:11 +0300

    .

--- src/Components/ContactMe/LeftDiv.tsx
+++ src/Components/ContactMe/LeftDiv.tsx
@@ -3,12 +3,12 @@ export default function LeftDiv() {
     <>
       <div className=" lg:w-2/3 flex flex-col  space-y-10  text-lg bg-violet-200 px-10 md:px-32 py-10 mb-10">
         <div className="flex-col space-y-3">
-          <p className="text-xl">Let's Connect</p>
+          <p className="text-xl">Let&apos;s Connect</p>
           <p className="text-5xl sm:text-6xl">Get In Touch</p>
           <p>
-            I'm currently open to taking on new projects, so feel free to reach
-            out to me anytime with anything you'd like to discuss. My inbox is
-            always open, 24/7.
+            I&apos;m currently open to taking on new projects, so feel free to
+            reach out to me anytime with anything you&apos;d like to discuss. My
+            inbox is always open, 24/7.
           </p>
         </div>
         <div className="flex-col">
--- src/Components/Welcome/AutoTyping.tsx
+++ src/Components/Welcome/AutoTyping.tsx
@@ -6,13 +6,13 @@ export default function AutoTyping() {
   return (
     <>
       <div className="text-2xl font-bold sm:text-4xl ">
-        <p className=" sm:mb-5">&nbsp;Hi, I'm Mohammad Jury</p>
+        <p className=" sm:mb-5">&nbsp;Hi, I&apos;m Mohammad Jury</p>
         <AuttoTyping />
         <p className=" sm:my-5 mb-3">&nbsp;Based In Nablus</p>
       </div>
       <p>
-        I'm a Computer Engineer, Interests in Software Engineering, especially
-        Full Stack Development.
+        I&apos;m a Computer Engineer, Interests in Software Engineering,
+        especially Full Stack Development.
       </p>
     </>
   );
