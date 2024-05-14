86c6f0cc4a5012a15b2475c783d82459305131e1
mohammadjury <mohammadjury00@gmail.com>
2024-05-14 05:43:27 +0300

    footer Done

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <>
-      <div className="flex justify-center text-white py-5 container-fullscreen bg-purple-800">
+      <div className="flex justify-center text-center text-white py-5 container-fullscreen bg-purple-800">
         Copyright © 2024 Mohammad Jury. All rights reserved.
       </div>
     </>
